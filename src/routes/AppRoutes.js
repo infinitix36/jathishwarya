3833840ac7988934a0ad37c608aef3af78434ec6
mathesh <matheshyogeswaran>
2023-01-31 15:56:38 -0800

    initial push

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Project from "../pages/Project";
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/project" element={<Project></Project>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default AppRoutes;
