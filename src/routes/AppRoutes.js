fa4b4b98e799d12c2711808285dc8df2be2f33a5
mathesh <matheshyogeswaran>
2023-02-01 06:47:16 +0530

    dashboard routing

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
 import Project from "../pages/Project";
 const AppRoutes = () => {
   return (
@@ -7,6 +8,7 @@ const AppRoutes = () => {
       <Routes>
         <Route exact path="/" element={<Project></Project>} />
         <Route exact path="/project" element={<Project></Project>} />
+        <Route exact path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </BrowserRouter>
   );
